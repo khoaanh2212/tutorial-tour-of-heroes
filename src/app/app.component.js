eb5d0e3952f8f39ffd15da12cf08e76f0bd71a49
Ka Nguyen <khoaanh2212@gmail.com>
2017-05-26 00:27:35 +0700

    Service

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -9,24 +9,19 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
-var hero_1 = require("./hero");
+var hero_service_1 = require("./hero.service");
 var AppComponent = (function () {
-    function AppComponent() {
+    function AppComponent(heroService) {
+        this.heroService = heroService;
         this.title = 'Tour of heroes';
-        this.heroes = [
-            new hero_1.Hero(11, 'Mr. Nice'),
-            new hero_1.Hero(12, 'Narco'),
-            new hero_1.Hero(13, 'Bombasto'),
-            new hero_1.Hero(14, 'Celeritas'),
-            new hero_1.Hero(15, 'Magneta'),
-            new hero_1.Hero(16, 'RubberMan'),
-            new hero_1.Hero(17, 'Dynama'),
-            new hero_1.Hero(18, 'Dr IQ'),
-            new hero_1.Hero(19, 'Magma'),
-            new hero_1.Hero(20, 'Tornado'),
-        ];
-        // this.selectedHero = this.heroes[0];
     }
+    AppComponent.prototype.ngOnInit = function () {
+        this.getHeroes();
+    };
+    AppComponent.prototype.getHeroes = function () {
+        var _this = this;
+        this.heroService.getHeroesSlowly().then(function (heroes) { return _this.heroes = heroes; });
+    };
     AppComponent.prototype.onSelect = function (hero) {
         this.selectedHero = hero;
     };
@@ -38,9 +33,10 @@ AppComponent = __decorate([
         /*template: `
          <h1>{{title}}</h1>
          <h2>{{hero}} details</h2>`,*/
-        templateUrl: './app.component.html'
+        templateUrl: './app.component.html',
+        providers: [hero_service_1.HeroService]
     }),
-    __metadata("design:paramtypes", [])
+    __metadata("design:paramtypes", [hero_service_1.HeroService])
 ], AppComponent);
 exports.AppComponent = AppComponent;
 //# sourceMappingURL=app.component.js.map
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,29 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Hero} from './hero';
+import {HeroService} from './hero.service';
 
 @Component({
   selector: 'my-app',
   /*template: `
    <h1>{{title}}</h1>
    <h2>{{hero}} details</h2>`,*/
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  providers: [HeroService]
 })
-export class AppComponent {
-  title: string;
+export class AppComponent implements OnInit {
+  title: string = 'Tour of heroes';
   selectedHero: Hero;
   heroes: Hero[];
 
-  constructor() {
-    this.title = 'Tour of heroes';
-    this.heroes = [
-      new Hero(11, 'Mr. Nice'),
-      new Hero(12, 'Narco'),
-      new Hero(13, 'Bombasto'),
-      new Hero(14, 'Celeritas'),
-      new Hero(15, 'Magneta'),
-      new Hero(16, 'RubberMan'),
-      new Hero(17, 'Dynama'),
-      new Hero(18, 'Dr IQ'),
-      new Hero(19, 'Magma'),
-      new Hero(20, 'Tornado'),
-    ];
-    // this.selectedHero = this.heroes[0];
+  constructor(private heroService: HeroService) {
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
